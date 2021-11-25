dac23d687484150b21f48fc62d99717cb1818bf9
Charlesdigital <charles.easton123@gmail.com>
2021-11-25 00:47:03 -0500

    iss spotter promise

---
+++ index2.js
@@ -0,0 +1,16 @@
+// const { fetchMyIP } = require("./iss_promised");
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => console.log(body));
+
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call
+nextISSTimesForMyLocation().then((passTimes) => {
+  printPassTimes(passTimes);
+});
